9b865fb7985686bb07eac9eaa69c07bb314d4e4d
parmentlovski <parmentelot.bryan@hotmail.fr>
2019-08-27 19:07:03 +0200

    sections sports et footer terminés

--- footer.php
+++ footer.php
@@ -1,7 +1,3 @@
-<div id="footer_background_before">
-    <img src="wp-content/themes/natureel/assets/img/background-dechire-footer.png" alt="">
-</div>
-
 <footer class="container-fluid">
     <div class="container">
         <div class="row d-flex justify-content-between align-items-center">
