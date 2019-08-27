650b21c534254fe3a181e03cee62a129012131e0
parmentlovski <parmentelot.bryan@hotmail.fr>
2019-08-27 09:01:24 +0200

    début section sports + footer

---
+++ footer.php
@@ -0,0 +1,17 @@
+</div><!-- /.container -->
+<footer class="container-fluid">
+    <div class="container">
+        <div class="row d-flex justify-content-between align-items-center">
+            <p class="col-12 col-sm-12 col-md-3 col-lg-3 col-xl-3">NatuReel tous droits réservées</p>
+            <p class="col-12 col-sm-12 col-md-3 col-lg-3 col-xl-3"><a href="#">Mentions légales</a></p>
+            <ul class="d-flex flex-row col-12 col-sm-12 col-md-1 col-lg-1 col-xl-1">
+                <li><a href="#"><i class="fab fa-facebook"></i></a></li>
+                <li><a href="#"><i class="fab fa-instagram"></i></a></li>
+            </ul>
+        </div>
+    </div>
+</footer>
+<?php wp_footer() ?>
+</body>
+
+</html>
---
+++ header.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="description" content="">
+<meta name="author" content="">
+<title>NatuReel</title>
+
+<!-- Ajout d'une nouvelle feuille de style qui sera spécifique à notre thème -->
+<link href="<?php bloginfo('template_directory'); ?>/style.css" rel="stylesheet">
+<!-- HTML5 shim et Respond.js pour supporter les éléments HTML5 pour les versions plus anciennes que Internet Explorer 9 -->
+<!--[if lt IE 9]>
+<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+<![endif]-->
+
+<?php wp_head(); ?>
+</head>
+    
+
