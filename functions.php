478900cc1a3fbbdd8f6286618424cec01150b7d6
parmentlovski <parmentelot.bryan@hotmail.fr>
2019-08-28 16:07:37 +0200

    uniformisation avec section concept et derniers articles

--- functions.php
+++ functions.php
@@ -20,7 +20,6 @@ wp_enqueue_script('script', '/wp-content/themes/natureel/script.js', array('jque
 wp_enqueue_script('script_navbar','https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js', array(), 1.0, true);
 wp_localize_script('script', 'ajaxurl', admin_url('admin-ajax.php'));
 wp_enqueue_script('leaflet', 'https://unpkg.com/leaflet@1.3.1/dist/leaflet.js');
-
 }
 
 ##################################### MENU ##################################
@@ -30,3 +29,12 @@ register_nav_menus(array('top' => 'Menu principal'));
 };
 
 add_action('init', 'register_my_menu');
+//Ajout des images a la une pour les articles de base de wordpress
+if ( function_exists( 'add_theme_support' ) ) {
+add_theme_support( 'post-thumbnails' );
+set_post_thumbnail_size( 150, 150, true ); // default Post Thumbnail dimensions (cropped)
+
+// additional image sizes
+// delete the next line if you do not need additional image sizes
+add_image_size( 'category-thumb', 300, 9999 ); //300 pixels wide (and unlimited height)
+}; 
