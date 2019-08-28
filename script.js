ab484135a10a207a304aa854a57f621da5e8b18e
Meriem Hadjal <meriem.hadjalchateur@gmail.com>
2019-08-28 08:37:39 +0000

    nouveau header mis en place

--- script.js
+++ script.js
@@ -1,10 +1,25 @@
 // VARIABLES
-var burger = document.querySelector('#test');
+var menu = document.querySelector('.container_menu');
+var burgerOpen = document.querySelector('.fa-bars');
+var burgerClose = document.querySelector('.fa-times');
 
-// EVENTS
-burger.addEventListener('click', function(){
+//EVENTS
 
-    burger.classList.toggle("is-active");
+burgerOpen.addEventListener('click', function(){
+
+  menu.style.top = "0";
+  menu.style.transition = "0.5s";
+  burgerOpen.style.display = "none";
+  burgerClose.style.display = "block";
+
 });
 
-// burger.classList.toggle("is-active");
+burgerClose.addEventListener('click', function(){
+
+    menu.style.top = "-1000px";
+    menu.style.transition = "0.5s";
+    burgerClose.style.display = "none";
+    burgerOpen.style.display = "block";
+  });
+
+
